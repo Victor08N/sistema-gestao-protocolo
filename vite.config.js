d53948acf3ad9c42fd03f3ca5ab455ca730bd338
Victor08N <victor.flrbjj@hotmail.com>
2025-11-14 08:04:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-  },
-  resolve: {
-    alias: {
-      firebase: "firebase/app",
-    },
-  },
-});
+  plugins: [react( )],
+  // ADICIONE ESTA LINHA:
+  base: '/', 
+})
